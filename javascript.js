9340bacf5bdba90bb6e8676bd30523b991c5afd0
Alex Angelico <alex.angelico@live.com>
2020-09-17 11:53:02 -0700

    converted JS file to discrete functions

--- javascript.js
+++ javascript.js
@@ -1,30 +1,40 @@
-var hikerName = "Hi " + prompt("What's your name?") + ", ";
-var greeting;
-var date = new Date();
-var weekDay = date.getDay();
-
-if (weekDay == 0)  {
-    greeting = "happy Sunday!";
-} else if (weekDay == 1)  {
-    greeting = "happy Monday!";
-} else if (weekDay == 2)  {
-    greeting = "happy Tuesday!";
-} else if (weekDay == 3)  {
-    greeting = "happy Wednesday!";
-} else if (weekDay == 4)  {
-    greeting = "happy Thursday!";
-} else if (weekDay == 5)  {
-    greeting = "happy Friday!";
-} else if (weekDay == 6)  {
-    greeting = "happy Saturday!";
-} else {
-    greeting = "you're hiking on another planet!";
+function questionHikerName() {
+    var hikerName = "Hi " + prompt("What's your name?") + ", ";
+        return hikerName;
 }
 
-if (confirm("Are you ready to hit the trail?")) {
-    response = " Let's find a place to go hiking!";
-} else {
-    response = " Here are some beautiful trails to plan for the next time you head out.";
+function greetingWeekDay() {
+    var greeting;
+    var date = new Date();
+    var weekDay = date.getDay();
+    
+    if (weekDay == 0)  {
+        greeting = "happy Sunday!";
+    } else if (weekDay == 1)  {
+        greeting = "happy Monday!";
+    } else if (weekDay == 2)  {
+        greeting = "happy Tuesday!";
+    } else if (weekDay == 3)  {
+        greeting = "happy Wednesday!";
+    } else if (weekDay == 4)  {
+        greeting = "happy Thursday!";
+    } else if (weekDay == 5)  {
+        greeting = "happy Friday!";
+    } else if (weekDay == 6)  {
+        greeting = "happy Saturday!";
+    } else {
+        greeting = "you're hiking on another planet!";
+    }
+        return greeting;
 }
 
-document.write(hikerName + greeting + response);
+function questionHiking(){
+    if (confirm("Are you ready to hit the trail?")) {
+        response = " Let's find a place to go hiking!";
+    } else {
+        response = " Here are some beautiful trails to plan for the next time you head out.";
+    }
+        return response;
+}
+
+document.write(questionHikerName() + greetingWeekDay() + questionHiking());
