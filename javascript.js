5575441cc8aa4bc2490bca15b4bb64e42b171da0
Alex Angelico <alex.angelico@live.com>
2020-09-18 09:39:07 -0700

    minor formatting changes

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-testVar = "TEST"
-
 function questionHikerName() {
     var hikerName = prompt("What's your name?");
     while (hikerName === "") {
@@ -15,19 +13,19 @@ function greetingWeekDay() {
     var date = new Date();
     var weekDay = date.getDay();
     
-    if (weekDay === 0)  {
+    if (weekDay === 0) {
         greeting = "happy Sunday!";
-    } else if (weekDay === 1)  {
+    } else if (weekDay === 1) {
         greeting = "happy Monday!";
-    } else if (weekDay === 2)  {
+    } else if (weekDay === 2) {
         greeting = "happy Tuesday!";
-    } else if (weekDay === 3)  {
+    } else if (weekDay === 3) {
         greeting = "happy Wednesday!";
-    } else if (weekDay === 4)  {
+    } else if (weekDay === 4) {
         greeting = "happy Thursday!";
-    } else if (weekDay === 5)  {
+    } else if (weekDay === 5) {
         greeting = "happy Friday!";
-    } else if (weekDay === 6)  {
+    } else if (weekDay === 6) {
         greeting = "happy Saturday!";
     } else {
         greeting = "you're hiking on another planet!";
@@ -36,7 +34,6 @@ function greetingWeekDay() {
 }
 
 function questionHiking() {
-    console.log(testVar)
     if (confirm("Are you ready to hit the trail?")) {
         response = " Let's find a place to go hiking!";
         return response;
@@ -54,7 +51,7 @@ function questionHiking() {
 }
 
 function numberGuess() {
-    var number = '72';
+    var number = "72";
     var guess = prompt("That's what I like to hear!\n\nOh, by the way, I've got a game to play while you're out hiking with a buddy: guess the number. Play between 1 and a 1,000,000 and see how many miles go by before one of you guesses--or pushes the other down a switchback! Before you move on to check out the trails, let's try a practice run...\n\nWhat number am I thinking of between 1 and 100?");
     for (var i = 1; i < 10; i++) {
         if (i < 3 && guess === number) {
